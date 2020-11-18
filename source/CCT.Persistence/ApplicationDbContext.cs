ccda3cc473606d53578ec526e3762b1f0a634436
NicoFSwee <56690947+NicoFSwee@users.noreply.github.com>
2020-11-18 17:33:07 +0100

    Add code behind in RazorPages

--- source/CCT.Persistence/ApplicationDbContext.cs
+++ source/CCT.Persistence/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace CCT.Persistence
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext()
         {
--- source/CCT.WebAPI/Pages/Persons.cshtml.cs
+++ source/CCT.WebAPI/Pages/Persons.cshtml.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CCT.Core.Contracts;
+using CCT.Core.Entities;
+using CCT.Persistence;
+using CCT.WebAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +13,28 @@ namespace CCT.WebAPI.Pages
 {
     public class PersonsModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IUnitOfWork _uow;
+        public Person[] PersonsOverview { get; set; }
 
+        public PersonsModel(IUnitOfWork unitOfWork)
+        {
+            _uow = unitOfWork;
+        }
+
+        public async Task OnGetAsync()
+        {
+            await PrepareDb();
+            PersonsOverview = await _uow.PersonRepository.GetAllPersonAsync();
+        }
+
+        private async Task PrepareDb()
+        {
+            await _uow.DeleteDatabaseAsync();
+
+            await _uow.PersonRepository.AddPersonAsync(new Person { FirstName = "Hannes", LastName = "Berger", PhoneNumber = "0650/8893128", RecordTime = DateTime.Now });
+            await _uow.PersonRepository.AddPersonAsync(new Person { FirstName = "Peter", LastName = "Auinger", PhoneNumber = "0650/1244418", RecordTime = DateTime.Now });
+            await _uow.PersonRepository.AddPersonAsync(new Person { FirstName = "Anna", LastName = "Berger", PhoneNumber = "0664/881141283", RecordTime = DateTime.Now });
+            await _uow.SaveChangesAsync();
         }
     }
 }
