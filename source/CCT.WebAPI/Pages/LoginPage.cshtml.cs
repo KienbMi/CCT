8011982a9ba545821d37881dfb2afffbbaf57fcf
KienbMi <m_kienberger@gmx.net>
2021-02-04 22:18:31 +0100

    Revert "cookies changes"
    This reverts commit 1ebe6451336d75781f3e2ed33a98dd1993052bc0.

--- source/CCT.WebAPI/Pages/LoginPage.cshtml.cs
+++ source/CCT.WebAPI/Pages/LoginPage.cshtml.cs
@@ -3,93 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CCT.APIService;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CCT.WebAPI.Pages
 {
-	public class LoginPageModel : PageModel
-	{
-		private SettingsClient _settingClient;
-		private string _password;
+    public class LoginPageModel : PageModel
+    {
+        private SettingsClient _settingCLient;
+        private string _password;
 
-		[BindProperty]
-		public string LastPage { get; set; }
+        [BindProperty]
+        public string LastPage { get; set; }
 
-		[BindProperty]
-		public string LabelTextWrongPassword { get; set; }
+        [BindProperty]
+        public string LabelTextWrongPassword { get; set; }
 
-		[BindProperty]
-		public string Password { get; set; }
-		public async Task<ActionResult> OnGetAsync(string handler)
-		{
-			LastPage = handler;
-			// ------- request cookie ---------------------
-			_settingClient = new SettingsClient();
-			var cookieValue = Request.Cookies["MyCookieId"];
-			string _pass;
-			try
-			{
-				_pass = await _settingClient.GetPasswordAsync();
-			}
-			catch (Exception)
-			{
-				return RedirectToPage("/LoginPage", "Index");
-			}
+        [BindProperty]
+        public string Password { get; set; }
+        public async Task<ActionResult> OnGetAsync(string handler)
+        {
+            LastPage = handler;
+            return Page();
+        }
 
-			// --------------------------------------------
-			if (handler == null || cookieValue == null || cookieValue != _pass)
-			{
-				return Page();
-			}
-			if(cookieValue == _pass)
-			{
-				if (LastPage == "Index")
-				{
-					return RedirectToPage("/RegistrationOverview", handler);
-				}
-				else if (LastPage == "Settings")
-				{
-					return RedirectToPage("/SettingsPage", handler);
-				}
-			}
+        public async Task<ActionResult> OnPostAsync(string handler)
+        {
+            _settingCLient = new SettingsClient();
 
-			return Page();
-		}
+            _password = await _settingCLient.GetPasswordAsync();
 
-		public async Task<ActionResult> OnPostAsync(string handler)
-		{
-			_settingClient = new SettingsClient();
+            if(Password == _password)
+            {
+                if(LastPage == "Index")
+                {
+                    return RedirectToPage("/RegistrationOverview", handler);
+                }
+                else if(LastPage == "Settings")
+                {
+                    return RedirectToPage("/SettingsPage", handler);
+                }
+            }
 
-			_password = await _settingClient.GetPasswordAsync();
-			
-			// ------------- set cookie ----------------------
-			CookieOptions option = new CookieOptions
-			{
-				Path = "/",
-				HttpOnly = false,
-				IsEssential = true,
-				Expires = DateTime.Now.AddMinutes(15)
-			};
-			Response.Cookies.Append("MyCookieId", $"{_password}", option);
-			// ---- using Microsoft.AspNetCore.Http ----------
+            LabelTextWrongPassword = "Passwort ist falsch!";
 
-			if (Password == _password)
-			{
-				if (LastPage == "Index")
-				{
-					return RedirectToPage("/RegistrationOverview", handler);
-				}
-				else if (LastPage == "Settings")
-				{
-					return RedirectToPage("/SettingsPage", handler);
-				}
-			}
-
-			LabelTextWrongPassword = "Passwort ist falsch!";
-
-			return Page();
-		}
-	}
+            return Page();
+        }
+    }
 }
--- source/CCT.WebAPI/Pages/SettingsPage.cshtml.cs
+++ source/CCT.WebAPI/Pages/SettingsPage.cshtml.cs
@@ -11,106 +11,90 @@ using ProblemDetails = CCT.APIService.ProblemDetails;
 
 namespace CCT.WebAPI.Pages
 {
-	public class SettingsPageModel : PageModel
-	{
-		private SettingsClient _settingClient;
-		public string LabelTextSuccess { get; set; }
-		[BindProperty]
-		public int StorageDuration { get; set; }
-		[BindProperty]
-		public string Password { get; set; }
-		[BindProperty]
-		public string WelcomeMessage { get; set; }
-		[BindProperty]
-		public string SelectedType { get; set; }
-		[BindProperty]
-		public List<SelectListItem> NfcReaderTypes { get; set; }
+    public class SettingsPageModel : PageModel
+    {
+        private SettingsClient _settingClient;
+        public string LabelTextSuccess { get; set; }
+        [BindProperty]
+        public int StorageDuration { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        [BindProperty]
+        public string WelcomeMessage { get; set; }
+        [BindProperty]
+        public string SelectedType { get; set; }
+        [BindProperty]
+        public List<SelectListItem> NfcReaderTypes { get; set; }
 
-		public async Task<ActionResult> OnGetAsync(string handler)
-		{
-			_settingClient = new SettingsClient();
-			// ------- request cookie ---------------------
-			var cookieValue = Request.Cookies["MyCookieId"];
-			string _pass = await _settingClient.GetPasswordAsync();
+        public async Task<ActionResult> OnGetAsync(string handler)
+        {
+            _settingClient = new SettingsClient();
 
-			if (cookieValue == null || cookieValue != _pass)
-			{
-				return RedirectToPage("/LoginPage", "Settings");
-			}
-			// --------------------------------------------
-			StorageDuration = await _settingClient.GetStorageDurationAsync();
-			WelcomeMessage = await _settingClient.GetWelcomeTextAsync();
-			Password = await _settingClient.GetPasswordAsync();
-			var type = await _settingClient.GetNfcReaderTypesAsync();
-			SelectedType = type.ToString();
+            StorageDuration = await _settingClient.GetStorageDurationAsync();
+            WelcomeMessage = await _settingClient.GetWelcomeTextAsync();
+            Password = await _settingClient.GetPasswordAsync();
+            var type = await _settingClient.GetNfcReaderTypesAsync();
+            SelectedType = type.ToString();
 
-			NfcReaderTypes = new List<SelectListItem>();
+            NfcReaderTypes = new List<SelectListItem>();
 
-			NfcReaderTypes.Add(new SelectListItem
-			{
-				Value = Core.NfcReaderType.RC522.ToString(),
-				Text = Core.NfcReaderType.RC522.ToString()
-			});
+            NfcReaderTypes.Add(new SelectListItem
+            {
+                Value = Core.NfcReaderType.RC522.ToString(),
+                Text = Core.NfcReaderType.RC522.ToString()
+            });
 
-			NfcReaderTypes.Add(new SelectListItem
-			{
-				Value = Core.NfcReaderType.uFr.ToString(),
-				Text = Core.NfcReaderType.uFr.ToString()
-			});
+            NfcReaderTypes.Add(new SelectListItem
+            {
+                Value = Core.NfcReaderType.uFr.ToString(),
+                Text = Core.NfcReaderType.uFr.ToString()
+            });
 
 
-			if (handler == null)
-			{
-				return RedirectToPage("/LoginPage", "Settings");
-			}
+            if (handler == null)
+            {
+                return RedirectToPage("/LoginPage", "Settings");
+            }
 
-			return Page();
-		}
+            return Page();
+        }
 
-		public async Task<ActionResult> OnPostAsync(string handler)
-		{
-			_settingClient = new SettingsClient();
-			// ------- request cookie ---------------------
-			var cookieValue = Request.Cookies["MyCookieId"];
-			string _pass = await _settingClient.GetPasswordAsync();
+        public async Task<ActionResult> OnPostAsync(string handler)
+        {
+            _settingClient = new SettingsClient();
 
-			if (cookieValue == null || cookieValue != _pass)
-			{
-				return RedirectToPage("/LoginPage", "Index");
-			}
-			// --------------------------------------------
-			try
-			{
-				await _settingClient.PostPasswordAsync(Password);
-				await _settingClient.PostStorageDurationAsync(StorageDuration);
-				await _settingClient.PostWelcomeTextAsync(WelcomeMessage);
-				await _settingClient.PostNfcReaderTypeAsync((APIService.NfcReaderType)Enum.Parse(typeof(APIService.NfcReaderType), SelectedType));
-			}
-			catch (ApiException<ProblemDetails> ex)
-			{
-				ModelState.AddModelError("ApiException", ex.Result.Detail);
-			}
+            try
+            {
+                await _settingClient.PostPasswordAsync(Password);
+                await _settingClient.PostStorageDurationAsync(StorageDuration);
+                await _settingClient.PostWelcomeTextAsync(WelcomeMessage);
+                await _settingClient.PostNfcReaderTypeAsync((APIService.NfcReaderType)Enum.Parse(typeof(APIService.NfcReaderType), SelectedType));
+            }
+            catch(ApiException<ProblemDetails> ex)
+            {
+                ModelState.AddModelError("ApiException", ex.Result.Detail);
+            }
 
-			NfcReaderTypes = new List<SelectListItem>();
+            NfcReaderTypes = new List<SelectListItem>();
 
-			NfcReaderTypes.Add(new SelectListItem
-			{
-				Value = Core.NfcReaderType.RC522.ToString(),
-				Text = Core.NfcReaderType.RC522.ToString()
-			});
+            NfcReaderTypes.Add(new SelectListItem
+            {
+                Value = Core.NfcReaderType.RC522.ToString(),
+                Text = Core.NfcReaderType.RC522.ToString()
+            });
 
-			NfcReaderTypes.Add(new SelectListItem
-			{
-				Value = Core.NfcReaderType.uFr.ToString(),
-				Text = Core.NfcReaderType.uFr.ToString()
-			});
+            NfcReaderTypes.Add(new SelectListItem
+            {
+                Value = Core.NfcReaderType.uFr.ToString(),
+                Text = Core.NfcReaderType.uFr.ToString()
+            });
 
-			var type = await _settingClient.GetNfcReaderTypesAsync();
-			SelectedType = type.ToString();
+            var type = await _settingClient.GetNfcReaderTypesAsync();
+            SelectedType = type.ToString();
 
-			LabelTextSuccess = "Einstellungen erfolgreich gespeichert!";
+            LabelTextSuccess = "Einstellungen erfolgreich gespeichert!";
 
-			return Page();
-		}
-	}
+            return Page();
+        }
+    }
 }
