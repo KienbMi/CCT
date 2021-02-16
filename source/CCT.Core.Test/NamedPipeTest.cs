fc9338275d84ccbc1fa815cc417741223de5ea06
KienbMi <m_kienberger@gmx.net>
2021-02-16 22:56:18 +0100

    remove T04 - T06

--- source/CCT.Core.Test/NamedPipeTest.cs
+++ source/CCT.Core.Test/NamedPipeTest.cs
@@ -62,59 +62,59 @@ namespace CCT.Core.Test
             Assert.AreEqual(expectedMessage2, actualMessage2);
         }
 
-        [TestMethod]
-        public void T04_SendMessageFromServer_Ok()
-        {
-            // Arrange
-            string pipeName = "T04pipe";
-            var server = new PipeServer(pipeName: pipeName);
-            var client = new PipeClient(pipeName: pipeName);
-            string expectedMessage = "Hallo ich bin der Server";
+        //[TestMethod]
+        //public void T04_SendMessageFromServer_Ok()
+        //{
+        //    // Arrange
+        //    string pipeName = "T04pipe";
+        //    var server = new PipeServer(pipeName: pipeName);
+        //    var client = new PipeClient(pipeName: pipeName);
+        //    string expectedMessage = "Hallo ich bin der Server";
 
-            // Act
-            server.SendMessage(expectedMessage);
-            string actualMessage = client.ReceiceMessage();
+        //    // Act
+        //    server.SendMessage(expectedMessage);
+        //    string actualMessage = client.ReceiceMessage();
 
-            // Assert
-            Assert.AreEqual(expectedMessage, actualMessage);
-        }
+        //    // Assert
+        //    Assert.AreEqual(expectedMessage, actualMessage);
+        //}
 
-        [TestMethod]
-        public void T05_SendMessageFromServerWithDelay_Ok()
-        {
-            // Arrange
-            string pipeName = "T05pipe";
-            var server = new PipeServer(pipeName: pipeName);
-            var client = new PipeClient(pipeName: pipeName);
-            string expectedMessage = "Hallo ich bin der Server";
+        //[TestMethod]
+        //public void T05_SendMessageFromServerWithDelay_Ok()
+        //{
+        //    // Arrange
+        //    string pipeName = "T05pipe";
+        //    var server = new PipeServer(pipeName: pipeName);
+        //    var client = new PipeClient(pipeName: pipeName);
+        //    string expectedMessage = "Hallo ich bin der Server";
 
-            // Act
-            server.SendMessage(expectedMessage);
-            Task.Delay(1000).Wait();
-            string actualMessage = client.ReceiceMessage();
+        //    // Act
+        //    server.SendMessage(expectedMessage);
+        //    Task.Delay(1000).Wait();
+        //    string actualMessage = client.ReceiceMessage();
 
-            // Assert
-            Assert.AreEqual(expectedMessage, actualMessage);
-        }
+        //    // Assert
+        //    Assert.AreEqual(expectedMessage, actualMessage);
+        //}
 
-        [TestMethod]
-        public void T06_SendMessageFromServerAndTwoReads_Ok()
-        {
-            // Arrange
-            string pipeName = "T06pipe";
-            var server = new PipeServer(pipeName: pipeName);
-            var client = new PipeClient(pipeName: pipeName);
-            string expectedMessage1 = "Hallo ich bin der Server";
-            string expectedMessage2 = "";
+        //[TestMethod]
+        //public void T06_SendMessageFromServerAndTwoReads_Ok()
+        //{
+        //    // Arrange
+        //    string pipeName = "T06pipe";
+        //    var server = new PipeServer(pipeName: pipeName);
+        //    var client = new PipeClient(pipeName: pipeName);
+        //    string expectedMessage1 = "Hallo ich bin der Server";
+        //    string expectedMessage2 = "";
 
-            // Act
-            server.SendMessage(expectedMessage1);
-            string actualMessage1 = client.ReceiceMessage();
-            string actualMessage2 = client.ReceiceMessage();
+        //    // Act
+        //    server.SendMessage(expectedMessage1);
+        //    string actualMessage1 = client.ReceiceMessage();
+        //    string actualMessage2 = client.ReceiceMessage();
 
-            // Assert
-            Assert.AreEqual(expectedMessage1, actualMessage1);
-            Assert.AreEqual(expectedMessage2, actualMessage2);
-        }
+        //    // Assert
+        //    Assert.AreEqual(expectedMessage1, actualMessage1);
+        //    Assert.AreEqual(expectedMessage2, actualMessage2);
+        //}
     }
 }
